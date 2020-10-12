c1217f4ee34c36d3e8ca7df15c09f1b762f83eee
burgundypie <kotyonok2001@mai.ru>
2020-10-12 13:31:30 +0300

    Симонова Арина

--- include/task1.h
+++ include/task1.h
@@ -1,1 +1,11 @@
-#pragma once
+#ifndef TASK1_H
+#define TASK1_H
+
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+
+#endif
---
+++ src/main1.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "task1.h"
+
+
+int main()
+{
+	double result = calc(2.0);
+	printf("Sqrt(%lf)=%lf\n", 2.0, result);
+	return 0;
+}
